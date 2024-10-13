0eb1b5a0202f9d010d13370125de61724eb8c546
Nachop51 <nachoperalta0@gmail.com>
2024-10-13 01:52:47 -0300

    fix light theme categories

--- src/build.ts
+++ src/build.ts
@@ -14,8 +14,11 @@ function filePath (name: string) {
 const themes: Array<{ name: string } & object> = [
   buildThemeFromColors({ colors: darkSchema, name: 'Nachop Dark', isBordered: true }) as { name: string },
   buildThemeFromColors({ colors: darkSchema, name: 'Nachop Dark', isBordered: false }) as { name: string },
-  buildThemeFromColors({ colors: lightSchema, name: 'Nachop Light', isBordered: true }) as { name: string },
-  buildThemeFromColors({ colors: lightSchema, name: 'Nachop Light', isBordered: false }) as { name: string }, buildThemeFromColors({ colors: facuSchema, name: 'Facu', isBordered: true }) as { name: string }, buildThemeFromColors({ colors: facuSchema, name: 'Facu', isBordered: false }) as { name: string }, buildThemeFromColors({ colors: piggySchema, name: 'Piggy', isBordered: true }) as { name: string }, buildThemeFromColors({ colors: piggySchema, name: 'Piggy', isBordered: false }) as { name: string }
+  buildThemeFromColors({ colors: lightSchema, name: 'Nachop Light', isBordered: true, type: 'light' }) as { name: string },
+  buildThemeFromColors({ colors: lightSchema, name: 'Nachop Light', isBordered: false, type: 'light' }) as { name: string },
+  buildThemeFromColors({ colors: facuSchema, name: 'Facu', isBordered: true }) as { name: string },
+  buildThemeFromColors({ colors: facuSchema, name: 'Facu', isBordered: false }) as { name: string },
+  buildThemeFromColors({ colors: piggySchema, name: 'Piggy', isBordered: true }) as { name: string }, buildThemeFromColors({ colors: piggySchema, name: 'Piggy', isBordered: false }) as { name: string }
 ]
 
 // Object.entries(nachopColors).forEach(([name, scheme]) => {
@@ -45,7 +48,8 @@ for (const theme of themes) {
   if (themeIndex === -1) {
     packageJson.contributes.themes.push({
       label: theme.name,
-      uiTheme: 'vs-dark',
+      // @ts-expect-error type always exists
+      uiTheme: theme.type === 'light' ? 'vs-light' : 'vs-dark',
       path: path.relative('.', fileName)
     })
     packageJsonChanged = true
